6ce3f3ef1ccd90116c3ddafd2f0e3ff7143e7705
bandhandut19 <bondon21081@gmail.com>
2024-06-28 13:34:55 +0600

    setup done (shadcn wroking properly.. this comit can be the primary one)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="w-4/5 mx-auto text-center p-20 border-2 border-red-600">
+        <h1>Bond ToDo</h1>
+        <Button>Lurag</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
